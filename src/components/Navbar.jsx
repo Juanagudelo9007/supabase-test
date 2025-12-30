ec088572b5183eb005651f0ef48c20d6ab2a1cd0
Juan Agudelo <verojuan9007@gmail.com>
2025-12-30 17:14:55 -0500

    greetings added, user name added to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router';
-import { AuthLogin } from '../context/UserAuth';
-import { useContext } from 'react';
+import React from "react";
+import { Link } from "react-router";
+import { AuthLogin } from "../context/UserAuth";
+import { useContext } from "react";
 
 const Navbar = () => {
-const {userOut} = useContext(AuthLogin)
+  const { userOut, userData } = useContext(AuthLogin);
 
   return (
     <div className="min-screen w-full">
-      <div className="w-full flex h-12 p-2 justify-center items-center gap-5 bg-black text-white">
+      <div className="relative w-full flex h-12 p-2 justify-center items-center gap-5 bg-black/80 text-white">
+        <h1 className="absolute left-3 capitalize text-green-400">
+          {userData?.user_metadata?.name}
+        </h1>
         <Link to={"/home"}>Home</Link>
         <Link to={"/myspace"}>My Space</Link>
+
         <button
-          className="bg-green-500 px-4 py-0.5 rounded-md  active:scale-70 transition-all duration-400"
+          className="absolute right-3 bg-green-500 px-4 py-0.5 rounded-md  active:scale-70 transition-all duration-400"
           onClick={userOut}
         >
           Log Out
@@ -20,6 +24,6 @@ const {userOut} = useContext(AuthLogin)
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { AuthLogin } from "../context/UserAuth";
+import { useContext } from "react";
 
 const Home = () => {
-  return (
-    <div>
-      Welcome Back
-    </div>
-  )
-}
+  const { userData } = useContext(AuthLogin);
+  const [showMessage, setShowMessage] = useState(true);
 
-export default Home
+  useEffect(() => {
+    const greetings = setTimeout(() => {
+      setShowMessage(false);
+    }, 3000);
+
+    return () => clearTimeout(greetings);
+  }, []);
+
+  return (
+    <div className="relative  w-full h-screen bg-black flex">
+      {showMessage && (
+        <h1 className="absolute top-6 right-[35%] text-3xl font-bold  capitalize text-transparent bg-clip-text bg-linear-to-t from-black/30 via-green-500 to-green-700 sm:hover:text-blue-400 ">
+          Hi, {userData?.user_metadata?.name}
+        </h1>
+      )}
+    </div>
+  );
+};
+
+export default Home;
